c3a870c23de9a7a4204e8b2417ac58de2722bc63
Colin <ab5169@coventry.ac.uk>
2021-09-23 13:41:43 +0100

    test django connects to elasticsearch

--- web/app/urls.py
+++ web/app/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    path("test_elastic/", include("test_elastic.urls")),
     path("admin/", admin.site.urls),
 ]
