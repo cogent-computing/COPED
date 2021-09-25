9bb0c1a190871c71486d6a3cabaf6e956a286316
Colin <ab5169@coventry.ac.uk>
2021-09-25 14:50:43 +0100

    switch back to python pip

--- voila/entrypoint.prod.sh
+++ voila/entrypoint.prod.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-voila --no-browser --server_url=/dashboards/ --base_url=/dashboards/ --Voila.tornado_settings="{'allow_origin': '*'}"
+voila --no-browser --server_url=/dashboards/ --base_url=/dashboards/ --Voila.tornado_settings="{'allow_origin': '*'}" /usr/share/voila/notebooks
 
 exec "$@"
