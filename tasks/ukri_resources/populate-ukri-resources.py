bcc26fdbef3b234fcd50306a12b579ff2dbe268c
Colin <ab5169@coventry.ac.uk>
2021-10-21 22:55:44 +0100

    add query to get resources into psql

--- tasks/ukri_resources/populate-ukri-resources.py
+++ tasks/ukri_resources/populate-ukri-resources.py
@@ -5,6 +5,7 @@
 import click
 import psycopg2
 import couchdb
+from psycopg2 import sql
 
 
 @click.command()
@@ -18,8 +19,6 @@ import couchdb
 @click.option("--postgres_user", envvar="POSTGRES_USER")
 @click.option("--postgres_password", envvar="POSTGRES_PASSWORD")
 @click.option("--postgres_db", envvar="POSTGRES_DB")
-@click.option("--postgres_resource_table", envvar="POSTGRES_RESOURCE_TABLE")
-@click.option("--postgres_type_table", envvar="POSTGRES_TYPE_TABLE")
 def main(
     couchdb_host,
     couchdb_port,
@@ -31,8 +30,6 @@ def main(
     postgres_user,
     postgres_password,
     postgres_db,
-    postgres_resource_table,
-    postgres_type_table,
 ):
 
     COUCHDB_URI = (
@@ -51,7 +48,20 @@ def main(
         with conn.cursor() as psql:
             for doc_id in couch:
                 doc = couch[doc_id]
-                print(doc_id, doc["item_type"])
+                item_type = doc["item_type"]
+                print(doc_id, item_type)
+
+                # add each item to the resource table with the correct resource type
+                query_string = """
+                    INSERT INTO coped_resource (document_id, resource_type_id)
+                    SELECT {doc_id}, crt.id
+                    FROM coped_couchdb_name AS ccn JOIN coped_resource_type AS crt
+                    ON ccn.id = crt.couchdb_id
+                    WHERE item_type = %s and ccn.name = %s
+                    ON CONFLICT DO NOTHING;
+                    """
+                query = sql.SQL(query_string).format(doc_id=sql.Literal(doc_id))
+                psql.execute(query, (item_type, couchdb_db))
 
 
 if __name__ == "__main__":
