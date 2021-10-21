a04585e24b3a8fdb7364bda946f409ad5b0c9e4c
Colin <ab5169@coventry.ac.uk>
2021-10-21 22:04:45 +0100

    run test for db connectivity

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -1,15 +1,10 @@
 #!/bin/sh
 
-if [ "$DATABASE" = "postgres" ]
-then
-  echo "Waiting for PostgreSQL..."
-
-  while ! nc -z $SQL_HOST $SQL_PORT; do
-    sleep 0.1
-  done
-
-  echo "PostgreSQL started"
-fi
+echo "Waiting for PostgreSQL..."
+while ! nc -z $SQL_HOST $SQL_PORT; do
+  sleep 0.1
+done
+echo "PostgreSQL started"
 
 # default to the PRODUCTION env to protect data
 ENV="${ENVIRONMENT:-PRODUCTION}"
