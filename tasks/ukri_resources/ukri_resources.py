801c646a5632c1e2ba7d73c8ff9bdd252d80a22e
Colin <ab5169@coventry.ac.uk>
2021-10-26 15:17:01 +0100

    use the shared code to transfer resources to psql

--- tasks/ukri_resources/ukri_resources.py
+++ tasks/ukri_resources/ukri_resources.py
@@ -3,40 +3,32 @@
 """Transfer IDs of resources from CouchDB to PostgreSQL"""
 
 import os
-import logging
 import click
+from shared.utils import coped_logging as log
 from shared.databases import couch_client
-from shared.databases import psql_query
-
-LOGLEVEL = os.environ.get("LOGLEVEL", "INFO").upper()
-logging.basicConfig(level=LOGLEVEL)
+from shared.tables import coped_allowed_items
+from shared.tables import coped_insert_resource
 
 
 @click.command()
 def main():
 
-    logging.info("Adding resources to PostgreSQL from CouchDB.")
+    log.info("Adding resources to PostgreSQL from CouchDB.")
     couch = couch_client()
 
     # Get the allowed item types from the DB.
-    # This ensures the foreign key constraint on the coped_resource table will be met.
-    query_string = "SELECT item_type FROM coped_resource_type;"
-    allowed_items = [row[0] for row in psql_query(query_string)]
-    logging.info(f"Allowed items: {allowed_items}")
+    # This allows filtering on known item types.
+    allowed_items = coped_allowed_items()
 
     for doc_id in couch:
         doc = couch[doc_id]
         item_type = doc["coped_meta"].get("item_type", None)
 
         if item_type not in allowed_items:
+            log.warning(f"Item type {item_type} of doc {doc_id} not allowed. Skipping.")
             continue
 
-        logging.info(f"adding resource ({doc_id}, {item_type}) to PostgreSQL")
-        query_string = """
-            INSERT INTO coped_resource (document_id, resource_type_id) VALUES (%s, %s)
-            ON CONFLICT DO NOTHING;
-            """
-        psql_query(query_string, values=(doc_id, item_type), fetch_results=False)
+        coped_insert_resource(doc_id, item_type)
 
 
 if __name__ == "__main__":
