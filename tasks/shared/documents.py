bf6bd5d8a4e93873973a7422c77ac0ceb505a406
Colin <ab5169@coventry.ac.uk>
2021-10-26 15:16:19 +0100

    add shared methods for couch and psql lookups

--- tasks/shared/documents.py
+++ tasks/shared/documents.py
@@ -1,42 +1,56 @@
 """Shared code for working with CoPED-managed documents inside CouchDB."""
 
-import logging
 from uuid import uuid4
 from datetime import datetime
+from functools import cache
 from deepdiff import DeepDiff
 from shared.databases import couch_client
+from shared.utils import coped_logging as log
 
 
 def different_docs(doc_1, doc_2):
     """Given two documents, are they different?"""
     diff = DeepDiff(dict(doc_1), dict(doc_2), ignore_order=True)
-    logging.debug(f"DOCUMENT DIFF = {diff}")
+    log.debug(f"DOCUMENT DIFF = {diff}")
     return bool(diff)
 
 
+@cache
+def get_all_ukri_ids():
+    """Find all ids of UKRI documents in the DB.
+
+    Use this rather than views to speed up checking UKRI ids.
+    Initial call takes similar time to a view. Subsequent calls use the cache."""
+    db = couch_client()
+    ids = []
+    for _id in db:
+        doc = db[_id]
+        if doc.get("coped_meta", {}).get("item_source", "") != "ukri-projects-spider":
+            continue
+        else:
+            ukri_id = doc.get("coped_meta", {}).get("item_id", None)
+            if ukri_id is not None:
+                ids.append((ukri_id, _id))
+    ukri_ids, coped_ids = zip(*ids)
+    return ukri_ids, coped_ids
+
+
+@cache
 def find_ukri_doc(ukri_id):
     """Search CouchDB for the given UKRI id."""
     db = couch_client()
+    ukri_ids, coped_ids = get_all_ukri_ids()
 
-    # TODO: create an index on `coped_meta.item_id` for speed.
-    query = {
-        "selector": {"coped_meta": {"item_id": ukri_id}},
-        "fields": ["_id"],
-    }
-
-    result = list(db.find(query))
-    item_found = bool(len(result))
-
-    if item_found:
+    if ukri_id in ukri_ids:
         # Return the full document to the caller.
-        _id = result[0]["_id"]
+        _id = coped_ids[ukri_ids.index(ukri_id)]
         return db[_id]
     else:
         return None
 
 
 def save_document(doc, update_message="document saved"):
-    """Save document and record a message in the `coped_meta.updates` field."""
+    """Save document in CouchDB and record a message in the `coped_meta.updates` field."""
 
     # Add a document id if it needs one (document creation)
     try:
