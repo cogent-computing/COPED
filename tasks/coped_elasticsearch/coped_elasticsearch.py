b5cff01a5e81b5d945e755d0b0a2fd066e986b71
Colin <ab5169@coventry.ac.uk>
2021-10-28 13:25:23 +0100

    allow reindexing in elastic

--- tasks/coped_elasticsearch/coped_elasticsearch.py
+++ tasks/coped_elasticsearch/coped_elasticsearch.py
@@ -11,7 +11,12 @@ from shared.databases import Couch
 
 
 @click.command()
-def main():
+@click.option(
+    "--refresh/--no_refresh",
+    default=False,
+    help="Remove all existing links before adding new ones.",
+)
+def main(refresh):
     """Index CoPED-managed CouchDB documents into Elasticsearch."""
 
     log.info("Indexing CoPed documents from CouchDB to Elasticsearch.")
@@ -20,20 +25,25 @@ def main():
         hosts=["elasticsearch"], http_auth=("elastic", "password")
     )
 
-    coped = Index("coped")
-    coped.settings(number_of_shards=1, number_of_replicas=0)
-    coped.create(ignore=400)
+    coped_idx = Index("coped")
 
-    @coped.document
+    if refresh:
+        log.warning("Deleting the 'coped' index from ElasticSearch.")
+        coped_idx.delete()
+
+    if not coped_idx.exists():
+        log.info("Creating new index 'coped' in ElasticSearch")
+        coped_idx.settings(number_of_shards=1, number_of_replicas=0)
+        coped_idx.create()
+
+    @coped_idx.document
     class CopedDoc(Document):
         pass
 
     couch = Couch()
     db = couch.db
-    coped_docs = couch.coped_docs()
-
     count = 0
-    for coped_doc in coped_docs:
+    for coped_doc in couch.coped_docs():
         doc = db[coped_doc.id]
         es_doc = CopedDoc(**doc)
         es_doc.meta.id = doc.id
