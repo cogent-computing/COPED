2897dd0279d0383c01c8ff044618f8da8d697114
Colin <ab5169@coventry.ac.uk>
2021-10-28 13:26:00 +0100

    allow refreshing all resources

--- tasks/coped_resources/coped_resources.py
+++ tasks/coped_resources/coped_resources.py
@@ -7,10 +7,17 @@ from shared.utils import coped_logging as log
 from shared.databases import Couch
 from shared.tables import coped_allowed_items
 from shared.tables import coped_insert_resource
+from shared.tables import coped_delete_all
 
 
 @click.command()
-def main():
+@click.option(
+    "--refresh/--no_refresh",
+    default=False,
+    help="WARNING destructive: removes all existing links before adding new ones.",
+)
+def main(refresh):
+    """Transfer IDs and resource types from CouchDB to PostgreSQL."""
 
     log.info("Adding resources to PostgreSQL from CouchDB.")
     couch = Couch()
@@ -19,6 +26,12 @@ def main():
     # Get the allowed item types from the DB.
     # This allows filtering on known item types.
     allowed_items = coped_allowed_items()
+
+    if refresh:
+        # Empty the resource table in PSQL first.
+        log.warning("Deleting all existing resources and relations from PostgreSQL.")
+        coped_delete_all("coped_relation")
+        coped_delete_all("coped_resource")
 
     for coped_doc in couch.coped_docs():
         doc_id = coped_doc.id
