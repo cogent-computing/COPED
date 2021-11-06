abbde7c0f7fae2327cc724bb7d24d858f4ab6a86
Colin <ab5169@coventry.ac.uk>
2021-11-06 19:54:49 +0000

    include urls and allow field choices in serialisers

--- services/web/api/serializers.py
+++ services/web/api/serializers.py
@@ -2,33 +2,82 @@ from rest_framework import serializers
 from core.models import Organisation, Person, Project, Fund
 
 
-class OrganisationSerializer(serializers.ModelSerializer):
+class DynamicFieldsModelSerializer(serializers.ModelSerializer):
+    """
+    A ModelSerializer that takes an additional `fields` argument that
+    controls which fields should be displayed.
+
+    https://www.django-rest-framework.org/api-guide/serializers/#dynamically-modifying-fields
+    """
+
+    def __init__(self, *args, **kwargs):
+        # Don't pass the 'fields' arg up to the superclass
+        fields = kwargs.pop("fields", None)
+
+        # Instantiate the superclass normally
+        super(DynamicFieldsModelSerializer, self).__init__(*args, **kwargs)
+
+        if fields is not None:
+            # Drop any fields that are not specified in the `fields` argument.
+            allowed = set(fields)
+            existing = set(self.fields)
+            for field_name in existing - allowed:
+                self.fields.pop(field_name)
+
+
+class OrganisationSerializer(DynamicFieldsModelSerializer):
     class Meta:
         model = Organisation
-        fields = ["id", "coped_id", "name", "address", "about"]
+        fields = ["url", "coped_id", "name", "address", "about"]
 
 
-class FundSerializer(serializers.ModelSerializer):
+class FundSerializer(DynamicFieldsModelSerializer):
+    organisation = serializers.HyperlinkedRelatedField(
+        view_name="organisation-detail", read_only=True
+    )
+
     class Meta:
         model = Fund
-        fields = ["id", "coped_id", "title", "about", "organisation"]
+        fields = ["url", "coped_id", "title", "about", "organisation"]
 
 
-class PersonSerializer(serializers.ModelSerializer):
+class PersonSerializer(DynamicFieldsModelSerializer):
+    organisation = serializers.HyperlinkedRelatedField(
+        view_name="organisation-detail", read_only=True
+    )
+    full_name = serializers.SerializerMethodField("get_full_name")
+
+    def get_full_name(self, obj):
+        return f"{obj.first_name} {obj.last_name}"
+
     class Meta:
         model = Person
-        fields = ["id", "coped_id", "first_name", "last_name", "organisation", "about"]
+        fields = [
+            "url",
+            "coped_id",
+            "first_name",
+            "last_name",
+            "organisation",
+            "about",
+            "full_name",
+        ]
 
 
 class ProjectSerializer(serializers.ModelSerializer):
-    funds = FundSerializer(many=True)
-    persons = PersonSerializer(many=True)
-    organisations = OrganisationSerializer(many=True)
+    organisations = OrganisationSerializer(
+        many=True, read_only=True, fields=("url", "coped_id", "name")
+    )
+    funds = FundSerializer(
+        many=True, read_only=True, fields=("url", "coped_id", "title")
+    )
+    persons = PersonSerializer(
+        many=True, read_only=True, fields=("url", "coped_id", "full_name")
+    )
 
     class Meta:
         model = Project
         fields = [
-            "id",
+            "url",
             "coped_id",
             "title",
             "description",
