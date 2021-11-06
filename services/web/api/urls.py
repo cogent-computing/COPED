7711cfff5d32dc8d2d6d31d96fd966b2714782d2
Colin <ab5169@coventry.ac.uk>
2021-11-06 19:55:13 +0000

    name url patterns for reverse lookups

--- services/web/api/urls.py
+++ services/web/api/urls.py
@@ -3,14 +3,19 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from api import views
 
 urlpatterns = [
-    path("persons/", views.PersonList.as_view()),
-    path("persons/<int:pk>/", views.PersonDetail.as_view()),
-    path("funds/", views.FundList.as_view()),
-    path("funds/<int:pk>/", views.FundDetail.as_view()),
-    path("organisations/", views.OrganisationList.as_view()),
-    path("organisations/<int:pk>/", views.OrganisationDetail.as_view()),
-    path("projects/", views.ProjectList.as_view()),
-    path("projects/<int:pk>/", views.ProjectDetail.as_view()),
+    path("persons/", views.PersonList.as_view(), name="person-list"),
+    path("persons/<int:pk>/", views.PersonDetail.as_view(), name="person-detail"),
+    path("funds/", views.FundList.as_view(), name="fund-list"),
+    path("funds/<int:pk>/", views.FundDetail.as_view(), name="fund-detail"),
+    path("organisations/", views.OrganisationList.as_view(), name="organisation-list"),
+    path(
+        "organisations/<int:pk>/",
+        views.OrganisationDetail.as_view(),
+        name="organisation-detail",
+    ),
+    path("projects/", views.ProjectList.as_view(), name="project-list"),
+    path("projects/<int:pk>/", views.ProjectDetail.as_view(), name="project-detail"),
+    path("", views.api_root),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
