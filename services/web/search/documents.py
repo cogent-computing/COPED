f859bddc4a01e9063a74a382fc1dde92b00405b4
Colin <ab5169@coventry.ac.uk>
2021-11-06 23:16:55 +0000

    simplify indexed document

--- services/web/search/documents.py
+++ services/web/search/documents.py
@@ -1,46 +1,20 @@
-from django.conf import settings
-from django_elasticsearch_dsl import Document, Index, fields
-from elasticsearch_dsl import analyzer
-
+from django_elasticsearch_dsl import Document
+from django_elasticsearch_dsl.registries import registry
 from core.models import Project
 
-# Name of the Elasticsearch index
-# INDEX = Index(settings.ELASTICSEARCH_INDEX_NAMES[__name__])
-INDEX = Index("project")
 
-# See Elasticsearch Indices API reference for available settings
-INDEX.settings(number_of_shards=1, number_of_replicas=1)
-
-# Custom analyser
-html_strip = analyzer(
-    "html_strip",
-    tokenizer="standard",
-    filter=["lowercase", "stop", "snowball"],
-    char_filter=["html_strip"],
-)
-
-# Document mapping
-@INDEX.doc_type
+@registry.register_document
 class ProjectDocument(Document):
-    """Project Elasticsearch document."""
+    """Elasticsearch mapping for project documents."""
 
-    id = fields.IntegerField(attr="id")
-
-    title = fields.TextField(
-        analyzer=html_strip,
-        fields={
-            "raw": fields.TextField(analyzer="keyword"),
-        },
-    )
-
-    description = fields.TextField(
-        analyzer=html_strip,
-        fields={
-            "raw": fields.TextField(analyzer="keyword"),
-        },
-    )
+    class Index:
+        name = "project"
+        settings = {"number_of_shards": 1, "number_of_replicas": 0}
 
     class Django(object):
-        """Inner nested class Django."""
 
         model = Project  # The core model associated with this Document
+        fields = [
+            "title",
+            "description",
+        ]
