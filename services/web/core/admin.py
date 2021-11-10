8741e86733b1f85f943ac98f59b35ad66c13bba4
Colin <ab5169@coventry.ac.uk>
2021-11-10 18:50:01 +0000

    make the raw JSON readable in admin

--- services/web/core/admin.py
+++ services/web/core/admin.py
@@ -6,6 +6,7 @@ from core.models.organisation import Organisation
 from core.models.project import Project, ProjectOrganisation, ProjectPerson, ProjectFund
 from core.models.raw_data import RawData
 
+
 # Define inlines for many-to-many relations
 
 
@@ -22,6 +23,9 @@ class ProjectPersonInline(admin.TabularInline):
 class ProjectFundInline(admin.TabularInline):
     model = ProjectFund
     extra = 1
+
+
+# Define the model admins themselves
 
 
 class ProjectAdmin(admin.ModelAdmin):
@@ -44,9 +48,50 @@ class FundAdmin(admin.ModelAdmin):
     inlines = (ProjectFundInline,)
 
 
+import json
+from pygments import highlight
+from pygments.lexers import JsonLexer
+from pygments.formatters import HtmlFormatter
+
+from django.contrib import admin
+from django.utils.safestring import mark_safe
+
+
+class RawDataAdmin(admin.ModelAdmin):
+    readonly_fields = ("data_prettified",)
+
+    def data_prettified(self, instance):
+        """Function to display pretty version of our data"""
+
+        # Convert the data to sorted, indented JSON
+        response = json.dumps(instance.json, sort_keys=True, indent=2)
+
+        # Truncate the data. Alter as needed
+        response = response[:5000]
+
+        # Get the Pygments formatter
+        formatter = HtmlFormatter(style="colorful")
+
+        # Highlight the data
+        response = highlight(response, JsonLexer(), formatter)
+
+        # Get the stylesheet
+        style = "<style>" + formatter.get_style_defs() + "</style><br>"
+
+        # Safe the output
+        return mark_safe(style + response)
+
+    data_prettified.short_description = "JSON Prettified"
+
+
+admin.site.register(RawData, RawDataAdmin)
+
+
+# Wire it all up
+
+
 admin.site.register(Person, PersonAdmin)
 admin.site.register(Project, ProjectAdmin)
 admin.site.register(Organisation, OrganisationAdmin)
 admin.site.register(Fund, FundAdmin)
-admin.site.register(RawData)
 admin.site.register(Permission)
