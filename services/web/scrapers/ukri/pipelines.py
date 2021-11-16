8bb4f568ed8acbde6386332b45984e88794ce8e4
Colin <ab5169@coventry.ac.uk>
2021-11-16 15:39:00 +0000

    remove old comment

--- services/web/scrapers/ukri/pipelines.py
+++ services/web/scrapers/ukri/pipelines.py
@@ -34,23 +34,3 @@ class SaveToDjangoPipeline:
                 record.save()
             else:
                 raise DropItem(url)
-
-        # try:
-        #     # Look for the item in the DB.
-        #     record = RawData.objects.get(url=url)
-        # except RawData.DoesNotExist:
-        #     record = None
-
-        # if record is None:
-        #     # Create a new record for this item.
-        #     logging.debug(f"Creating new record for {url}.")
-        #     RawData.objects.create(bot=spider.name, url=url, json=item)
-        # else:
-        #     # Check record for differences and update if there are any.
-        #     diff = DeepDiff(dict(item), dict(record.json), ignore_order=True)
-        #     if diff:
-        #         logging.debug(f"Updating existing record for {url}.")
-        #         record.json = item
-        #         record.save()
-        #     else:
-        #         raise DropItem(url)
