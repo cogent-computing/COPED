89d0f6499f7ee892175d2921da86cee68699632f
Colin <ab5169@coventry.ac.uk>
2021-11-16 11:31:42 +0000

    use get_or_create to avoid try block

--- services/web/scrapers/ukri/pipelines.py
+++ services/web/scrapers/ukri/pipelines.py
@@ -19,22 +19,38 @@ class SaveToDjangoPipeline:
         """Upsert the item."""
 
         url = item.get("href")
-        try:
-            # Look for the item in the DB.
-            record = RawData.objects.get(url=url)
-        except RawData.DoesNotExist:
-            record = None
-
-        if record is None:
-            # Create a new record for this item.
-            logging.debug(f"Creating new record for {url}.")
-            RawData.objects.create(bot=spider.name, url=url, json=item)
+        record, created = RawData.objects.get_or_create(url=url)
+        if created:
+            logging.debug(f"Created new record for {url}")
+            record.bot = spider.name
+            record.json = item
+            record.save()
         else:
             # Check record for differences and update if there are any.
             diff = DeepDiff(dict(item), dict(record.json), ignore_order=True)
             if diff:
-                logging.debug(f"Updating existing record for {url}.")
+                logging.debug(f"Updating existing record for {url}")
                 record.json = item
                 record.save()
             else:
                 raise DropItem(url)
+
+        # try:
+        #     # Look for the item in the DB.
+        #     record = RawData.objects.get(url=url)
+        # except RawData.DoesNotExist:
+        #     record = None
+
+        # if record is None:
+        #     # Create a new record for this item.
+        #     logging.debug(f"Creating new record for {url}.")
+        #     RawData.objects.create(bot=spider.name, url=url, json=item)
+        # else:
+        #     # Check record for differences and update if there are any.
+        #     diff = DeepDiff(dict(item), dict(record.json), ignore_order=True)
+        #     if diff:
+        #         logging.debug(f"Updating existing record for {url}.")
+        #         record.json = item
+        #         record.save()
+        #     else:
+        #         raise DropItem(url)
