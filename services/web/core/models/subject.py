b9aba4e00e021348f695ce86b3335b153022dd9e
Colin <ab5169@coventry.ac.uk>
2021-11-19 20:11:05 +0000

    create a subject model for categorising projects

---
+++ services/web/core/models/subject.py
@@ -0,0 +1,31 @@
+from django.db import models
+from uuid import uuid4
+from .external_link import ExternalLink
+
+
+class Subject(models.Model):
+    """Subjects for categorising text.
+
+    Since crawled data often include off-topic items we need to pre-process them
+    to extract a good guess regarding their primary subject(s).
+
+    Assigned subjects can then be used as a pre-filter before creating records in
+    the main CoPED project table."""
+
+    coped_id = models.UUIDField(default=uuid4, editable=False, verbose_name="CoPED ID")
+    label = models.CharField(
+        max_length=64, help_text="Short description of the subject."
+    )
+    external_link = models.ForeignKey(
+        ExternalLink,
+        on_delete=models.SET_NULL,
+        null=True,
+        help_text="Link to an ontology.",
+    )
+
+    class Meta:
+        db_table = "coped_subject"
+        ordering = ["label"]
+
+    def __str__(self):
+        return self.label
