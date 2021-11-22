1faf2a9e3ec5fcdf0185c852bafe20ac8266ff51
Colin <ab5169@coventry.ac.uk>
2021-11-22 08:23:56 +0000

    subject tagging improvements

--- services/web/tasks/subject_tagger.py
+++ services/web/tasks/subject_tagger.py
@@ -12,6 +12,7 @@ See the following links for details:
     - https://ai.finto.fi/v1/ui/
 """
 
+import time
 import json
 import os
 import sys
@@ -29,44 +30,74 @@ from core.models import ExternalLink
 import requests
 
 
-def tag_projects_with_subjects(exclude_already_tagged=True):
+def tag_projects_with_subjects(exclude_already_tagged=True, limit=None):
     """Send project descriptions to the subject suggestion model at Finto.
 
-    Updates project records by adding the suggested subjects and scores.
-    Adds each subject to the DB if it does not already exist, and also
-    adds an external link to an ontology DB for each subject term."""
+    Update project records by adding the suggested subjects and scores.
+    Add each subject to the DB if it does not already exist, and add
+    an external link to an ontology DB for each subject term stored."""
 
     api_url = "https://ai.finto.fi/v1/projects/yso-en/suggest"
+    # TODO: centralise the user agent setting for use in other external-service-facing tasks.
+    headers = {
+        "User-Agent": "coped/0.1 (Copedbot/0.1; Catalogue of Projects on Energy Data; https://coped.coventry.ac.uk)"
+    }
 
-    for project in Project.objects.all()[:50]:
+    total_projects = Project.objects.count()
+    if limit is None:
+        limit = total_projects  # number of projects to tag
 
-        if exclude_already_tagged and project.subjects.exists():
-            continue
+    with requests.session() as connection:
+        connection.headers.update(headers)
 
-        print(project.title)
-        payload = {
-            "project_id": "yso-en",  # Finto English language subject classifier
-            "text": project.description,
-            "limit": 20,
-        }
-        r = requests.post(api_url, data=payload)
-        results = json.loads(r.content)["results"]
+        count = 0
+        for project in Project.objects.all()[:limit]:
 
-        with transaction.atomic():
-            for r in results:
-                label, score, uri = r["label"], r["score"], r["uri"]
-                link, _ = ExternalLink.objects.get_or_create(
-                    link=uri,
-                    defaults={"description": "Finto term ontology"},
+            count += 1
+
+            if exclude_already_tagged and project.subjects.exists():
+                print(
+                    f"Project {count} of {total_projects} (id={project.id}) already tagged. Skipping."
                 )
-                subject, _ = Subject.objects.get_or_create(
-                    label=label,
-                    defaults={"external_link": link},
+                continue
+
+            print(
+                f"Project {count} of {total_projects} (id={project.id}) not tagged. Tagging: {project.title[:30]}..."
+            )
+
+            results = []
+            try:
+                payload = {
+                    "project_id": "yso-en",  # Finto English language subject classifier
+                    "text": project.description,
+                    "limit": 20,
+                }
+                r = connection.post(api_url, data=payload, headers=headers, timeout=10)
+                results = json.loads(r.content)["results"]
+            except requests.exceptions.RequestException as e:
+                print(
+                    f"Request to tag project {project.id} failed with exception:\n{e}\n"
                 )
-                ProjectSubject.objects.update_or_create(
-                    project=project, subject=subject, defaults={"score": score}
-                )
+                continue
+
+            with transaction.atomic():
+                for r in results:
+                    label, score, uri = r["label"], r["score"], r["uri"]
+                    link, _ = ExternalLink.objects.get_or_create(
+                        link=uri,
+                        defaults={"description": "Finto term ontology"},
+                    )
+                    subject, _ = Subject.objects.get_or_create(
+                        label=label,
+                        defaults={"external_link": link},
+                    )
+                    ProjectSubject.objects.update_or_create(
+                        project=project, subject=subject, defaults={"score": score}
+                    )
+
+            # ease off between API hits to avoid saturating the remote server
+            time.sleep(2)
 
 
 if __name__ == "__main__":
-    tag_projects_with_subjects(exclude_already_tagged=True)
+    tag_projects_with_subjects(exclude_already_tagged=True, limit=1017)
