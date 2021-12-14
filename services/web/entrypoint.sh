5eea5c45837aa7debb9a8b56b25f5127c0240902
Colin <ab5169@coventry.ac.uk>
2021-12-14 14:11:25 +0000

    ensure we index to elastic on launch

--- services/web/entrypoint.sh
+++ services/web/entrypoint.sh
@@ -12,4 +12,8 @@ echo "Collecting static files for Nginx..."
 python manage.py collectstatic --no-input --clear
 echo "Static file collection complete"
 
+echo "Building elasticsearch indexes..."
+python manage.py search_index --rebuild -f
+echo "Elasticsearch indexes complete"
+
 exec "$@"
