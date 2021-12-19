588623b47986f54346b39424b480438fa84c01de
Colin <ab5169@coventry.ac.uk>
2021-12-19 20:55:05 +0000

    Revert "remove old users code"
    This reverts commit 7e8180aec4dab74418ec3c2ff0b40407c0effb48.

---
+++ services/web/users/forms.py
@@ -0,0 +1,14 @@
+from django.forms import ModelForm, EmailInput, PasswordInput
+from django import forms
+from .models import User
+
+
+class RegisterForm(ModelForm):
+    first_name = forms.CharField(max_length=100, label="First Name")
+    last_name = forms.CharField(max_length=100, label="Last Name")
+    email = forms.CharField(widget=EmailInput)
+    password = forms.CharField(widget=PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ["email", "password", "first_name", "last_name"]
---
+++ services/web/users/models.py
@@ -0,0 +1,123 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
+from django.contrib.postgres.fields import ArrayField
+from django.contrib.auth.base_user import BaseUserManager
+
+from uuid import uuid4
+
+# class CustomUser(AbstractBaseUser, PermissionsMixin):
+#     email = models.EmailField(_('email address'), unique=True)
+#     first_name = models.CharField(max_length=250)
+#     last_name = models.CharField(max_length=250)
+#     is_staff = models.BooleanField(default=False)
+#     is_active = models.BooleanField(default=True)
+#     u_id = models.UUIDField(primary_key=False, default=uuid.uuid4, editable=True)
+#     date_joined = models.DateTimeField(default=timezone.now)
+#     res_perm_list =  ArrayField(
+#             models.UUIDField(primary_key=False, default=uuid.uuid4, editable=True),
+#             size=100, default=list, null=True
+#         )
+#     subscriptions =  ArrayField(
+#             models.UUIDField(primary_key=False, default=uuid.uuid4, editable=True),
+#             size=100, default=list, null=True
+#         )
+#     USERNAME_FIELD = 'email'
+#     REQUIRED_FIELDS = ['first_name','last_name']
+
+#     objects = CustomUserManager()
+
+#     def __str__(self):
+#         return self.email
+
+
+# class PasswChange(models.Model):
+#     """
+#     Model to handle password change
+#     """
+#     email = models.EmailField(_('email address'))
+#     sender = models.UUIDField(primary_key=False, default=uuid.uuid4, editable=True)
+#     date_requested = models.DateTimeField(default=timezone.now)
+#     secret = models.CharField(max_length=50)
+#     used = models.BooleanField(default=False)
+#     # Metadata
+#     class Meta:
+#         ordering = ['email', 'sender', '-date_requested']
+
+#     def __str__(self):
+#         """String for representing the MyModelName object (in Admin site etc.)."""
+#         return "Passw_Change:" + str(self.email) + " on:" + str(self.date_requested) + "secret: " + str(self.secret)
+
+
+class UserManager(BaseUserManager):
+    """
+    Custom user model manager where email is the unique identifiers
+    for authentication instead of usernames.
+    """
+
+    def create_user(self, email, password, first_name, last_name, **extra_fields):
+        """
+        Create and save a User with the given email and password.
+        """
+        if not email:
+            raise ValueError(_("The Email must be set"))
+        email = self.normalize_email(email)
+        user = self.model(
+            email=email, first_name=first_name, last_name=last_name, **extra_fields
+        )
+        user.set_password(password)
+        # u_id = c_con.create_user(user)
+        # user.u_id = u_id
+        # permission to own page
+        # user.res_perm_list = [user.u_id]
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, first_name, last_name, **extra_fields):
+        """
+        Create and save a SuperUser with the given email and password.
+        """
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError(_("Superuser must have is_staff=True."))
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError(_("Superuser must have is_superuser=True."))
+        return self.create_user(email, password, first_name, last_name, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    coped_id = models.UUIDField(default=uuid4, editable=False, verbose_name="CoPED ID")
+    email = models.EmailField(_("email address"), unique=True)
+    first_name = models.CharField(max_length=250)
+    last_name = models.CharField(max_length=250)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+    # res_perm_list = ArrayField(
+    #     models.UUIDField(primary_key=False, default=uuid.uuid4, editable=True),
+    #     size=100,
+    #     default=list,
+    #     null=True,
+    # )
+    # subscriptions = ArrayField(
+    #     models.UUIDField(primary_key=False, default=uuid.uuid4, editable=True),
+    #     size=100,
+    #     default=list,
+    #     null=True,
+    # )
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name"]
+
+    objects = UserManager()
+
+    class Meta:
+        # Set the custom user model to have the standard Django database table name
+        db_table = "auth_user"
+
+    def __str__(self):
+        return self.email
