64724a279eeb5dd77c2fa7a0fcbb0c9a8918bcc3
Colin <ab5169@coventry.ac.uk>
2021-12-24 22:56:14 +0000

    recover metabase to a known state

--- dbdata/repopulate_db.sh
+++ dbdata/repopulate_db.sh
@@ -4,17 +4,23 @@
 # Re-populate the development database on demand.
 # Ensure the database name below (default = 'coped_development') matches what your development environment uses.
 
-echo "Stopping containers and repopulating the db."
-
+echo "Stopping containers."
 docker-compose down
 docker-compose up -d db
+
+echo "Repopulating CoPED database from backup."
 docker-compose exec db psql -d postgres -c "SELECT pg_terminate_backend(psa.pid) FROM pg_stat_activity psa WHERE datname = 'coped_development' AND pid <> pg_backend_pid();"
 docker-compose exec db psql -d postgres -c "DROP DATABASE coped_development;"
 docker-compose exec db psql -d postgres -c "CREATE DATABASE coped_development;"
 docker-compose exec db psql -d coped_development < dbdata/coped.backup.sql
 
-echo "Data updated. Starting web container."
+echo "Repopulating CoPED database from backup."
+docker-compose exec db psql -d postgres -c "SELECT pg_terminate_backend(psa.pid) FROM pg_stat_activity psa WHERE datname = 'metabase' AND pid <> pg_backend_pid();"
+docker-compose exec db psql -d postgres -c "DROP DATABASE metabase;"
+docker-compose exec db psql -d postgres -c "CREATE DATABASE metabase;"
+docker-compose exec db psql -d coped_development < dbdata/metabase.backup.sql
 
-docker-compose up -d web
+echo "Data updated. Starting CoPED services."
+docker-compose up -d
 
-echo "Web container now running."
+echo "Complete. You can now use the CoPED application."
