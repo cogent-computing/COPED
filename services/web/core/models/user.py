7a291126cf09351ba372c65162c4fbc6cfc6a90a
Colin <ab5169@coventry.ac.uk>
2022-01-07 11:22:51 +0000

    refactor custom user model; add metabase id field

--- services/web/core/models/user.py
+++ services/web/core/models/user.py
@@ -1,4 +1,5 @@
-"""Custom User model for CoPED.
+"""
+Custom User model for CoPED.
 
 See https://docs.djangoproject.com/en/3.2/topics/auth/customizing/ for details
 and considerations when using a custom model. In particular, note the following.
@@ -10,67 +11,20 @@ and considerations when using a custom model. In particular, note the following.
 > When you define a foreign key or many-to-many relations to the user model, you
 > should specify the custom model using the AUTH_USER_MODEL setting.
 
-There are various other requirements, on which see the link above.
+There are various other requirements, related to how Django-Registration works with
+the User model. See https://django-registration.readthedocs.io/en/3.2/custom-user.html
+for specifics on this.
 """
-
-# class User(AbstractBaseUser):
-#     class Meta:
-#         # Set the custom user model to have the standard Django database table name
-#         db_table = "auth_user"
 
 from django.conf import settings
 from django.db import models
 from django.core.mail import send_mail
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
-from django.contrib.auth.base_user import BaseUserManager
-from django.contrib.postgres.fields import ArrayField
+from django.contrib.auth.models import UserManager
 from django.utils.translation import gettext_lazy as _
 from django.utils import timezone
 from uuid import uuid4
-
-# # from coped_controller.coped_controller_local import Coped_Controller
-
-# # c_con = Coped_Controller()
-
-
-class UserManager(BaseUserManager):
-    """
-    Custom user model manager where email is the unique identifiers
-    for authentication instead of usernames.
-    """
-
-    def create_user(self, email, password, first_name, last_name, **extra_fields):
-        """
-        Create and save a User with the given email and password.
-        """
-        if not email:
-            raise ValueError(_("The Email must be set"))
-        email = self.normalize_email(email)
-        user = self.model(
-            email=email, first_name=first_name, last_name=last_name, **extra_fields
-        )
-        user.set_password(password)
-        # u_id = c_con.create_user(user)
-        # user.u_id = u_id
-        # permission to own page
-        # user.res_perm_list = [user.u_id]
-        user.save()
-        return user
-
-    def create_superuser(self, email, password, first_name, last_name, **extra_fields):
-        """
-        Create and save a SuperUser with the given email and password.
-        """
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
-        extra_fields.setdefault("is_active", True)
-
-        if extra_fields.get("is_staff") is not True:
-            raise ValueError(_("Superuser must have is_staff=True."))
-        if extra_fields.get("is_superuser") is not True:
-            raise ValueError(_("Superuser must have is_superuser=True."))
-        return self.create_user(email, password, first_name, last_name, **extra_fields)
 
 
 class User(AbstractBaseUser, PermissionsMixin):
@@ -82,22 +36,15 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     date_joined = models.DateTimeField(default=timezone.now)
-    # res_perm_list = ArrayField(
-    #     models.UUIDField(primary_key=False, default=uuid.uuid4, editable=True),
-    #     size=100,
-    #     default=list,
-    #     null=True,
-    # )
-    # subscriptions = ArrayField(
-    #     models.UUIDField(primary_key=False, default=uuid.uuid4, editable=True),
-    #     size=100,
-    #     default=list,
-    #     null=True,
-    # )
-    USERNAME_FIELD = "username"
-    REQUIRED_FIELDS = ["first_name", "last_name"]
 
-    objects = UserManager()
+    # A cross-db "foreign key" field is used to associate this user with a Metabase profile:
+    metabase_id = models.IntegerField(
+        editable=False, null=True, verbose_name="Metabase ID"
+    )
+
+    USERNAME_FIELD = "username"  # Needed for Django-Registration
+
+    objects = UserManager()  # Every custom User model needs an explicit manager
 
     class Meta:
         # Set the custom user model to have the standard Django database table name
@@ -106,10 +53,11 @@ class User(AbstractBaseUser, PermissionsMixin):
     def email_user(self, subject, message, from_email=None, **kwargs):
         if from_email is None:
             from_email = settings.DEFAULT_FROM_EMAIL
-        print(f"SENDING EMAIL TO USER WITH ID {self.id}")
-        print("subject:", subject)
-        print("message:", message)
-        print("from_email", from_email)
+        if settings.DEBUG:
+            print(f"SENDING EMAIL TO USER WITH ID {self.id}")
+            print("subject:", subject)
+            print("message:", message)
+            print("from_email", from_email)
         send_mail(
             subject=subject,
             message=message,
