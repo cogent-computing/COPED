46df2ed28d0404d317651e488bdb2c5c22334bb2
Colin <ab5169@coventry.ac.uk>
2022-01-07 11:23:24 +0000

    refactor signal handlers for registration and password

--- services/web/core/signals.py
+++ services/web/core/signals.py
@@ -2,62 +2,82 @@
 Application signal handlers.
 """
 
-import requests
 import os
+import requests
+from django.conf import settings
 from django.contrib.auth import get_user_model
 
+METABASE_API_URL = "http://metabase:3000/api"
 
-def user_registration_handler(sender, **kwargs):
-    """
-    Add a user profile into Metabase when a user confirms and activates their CoPED account.
 
-    This allows user access to the functionality of Metabase in a more seamless way.
+def get_metabase_superuser_access_token():
     """
-    print("A user was registered!")
-    print("Sender", sender)
-    print("kwargs", kwargs)
-    request = kwargs.get("request")
-    print("request", request)
-    body = request.POST
-    print("body", body)
-    password = body.get("password1")
-    print("password", password)
+    Each request to add a new user to Metabase must be authenticated using a token.
+
+    TODO: save this in the DB during its validity period to avoid excessive token requests.
+    """
     metabase_superuser_email = os.environ.get("METABASE_SUPERUSER_EMAIL")
     metabase_superuser_password = os.environ.get("METABASE_SUPERUSER_PASSWORD")
-    metabase_api_url = "http://metabase:3000/api"
+    metabase_token_url = f"{METABASE_API_URL}/session"
     request_data = {
         "username": metabase_superuser_email,
         "password": metabase_superuser_password,
     }
-    r = requests.post(f"{metabase_api_url}/session", json=request_data)
+    r = requests.post(metabase_token_url, json=request_data)
     token = r.json().get("id")
-    print("Metabase admin session token", token)
+    return token
+
+
+def user_registration_handler(sender, **kwargs):
+    """
+    Add a user profile into Metabase when a user registers a CoPED account.
+
+    This allows user access to the functionality of Metabase in a more seamless way.
+    Stores the Metabase user ID in the field User.metabase_id for reference later.
+    """
 
     coped_user = kwargs["user"]
+
+    first_name = coped_user.first_name if coped_user.first_name else coped_user.username
+    last_name = coped_user.last_name if coped_user.last_name else coped_user.username
+    email = coped_user.email
+
+    request = kwargs["request"]
+    body = request.POST
+    password = body.get("password1")
+
     user_to_save_in_metabase = {
-        "first_name": coped_user.first_name
-        if coped_user.first_name
-        else coped_user.username,
-        "last_name": coped_user.last_name
-        if coped_user.last_name
-        else coped_user.username,
-        "email": coped_user.email,
+        "first_name": first_name,
+        "last_name": last_name,
+        "email": email,
         "password": password,
     }
-    print("User to save", user_to_save_in_metabase)
 
+    if settings.DEBUG:
+        print("User to save in Metabase", user_to_save_in_metabase)
+
+    token = get_metabase_superuser_access_token()
     auth = {"X-Metabase-Session": token}
+
+    # Add the user to Metabase.
     r = requests.post(
-        f"{metabase_api_url}/user", json=user_to_save_in_metabase, headers=auth
+        f"{METABASE_API_URL}/user", json=user_to_save_in_metabase, headers=auth
     )
     result = r.json()
-    print("Response from Metabase API", result)
+    if settings.DEBUG:
+        print("Response from Metabase API", result)
+
+    # Record the Metabase user ID in the CoPED user record.
+    metabase_id = result["id"]
+    coped_user.metabase_id = metabase_id
+    coped_user.save(update_fields=["metabase_id"])
 
 
 def detect_password_change(sender, instance, **kwargs):
     """
-    Checks if the user changed their password
+    Check if the user changed their password and update it in Metabase if so.
     """
+
     if instance._password is None:
         return
 
@@ -66,30 +86,22 @@ def detect_password_change(sender, instance, **kwargs):
     except get_user_model().DoesNotExist:
         return
 
-    print("password changed for user", user)
-    print("old pass", user.password)
-    print("new pass", instance._password)
+    if not user.metabase_id:
+        if settings.DEBUG:
+            print(f"No metabase_id for user {user}. Cannot update password.")
+        return
 
-    metabase_superuser_email = os.environ.get("METABASE_SUPERUSER_EMAIL")
-    metabase_superuser_password = os.environ.get("METABASE_SUPERUSER_PASSWORD")
-    metabase_api_url = "http://metabase:3000/api"
-    request_data = {
-        "username": metabase_superuser_email,
-        "password": metabase_superuser_password,
-    }
-    r = requests.post(f"{metabase_api_url}/session", json=request_data)
-    token = r.json().get("id")
-    print("Metabase admin session token", token)
+    if settings.DEBUG:
+        print(f"Changing Metabase password for user: {user}")
 
-    new_password = {
-        "password": instance._password,
-    }
-
+    new_password = {"password": instance._password}
+    token = get_metabase_superuser_access_token()
     auth = {"X-Metabase-Session": token}
     r = requests.put(
-        f"{metabase_api_url}/user/11/password",
+        f"{METABASE_API_URL}/user/{user.metabase_id}/password",
         json=new_password,
         headers=auth,
     )
     result = r.json()
-    print("Response from Metabase API", result)
+    if settings.DEBUG:
+        print("Response from Metabase API", result)
