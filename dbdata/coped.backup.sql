e970bf2b58e03e86e67e01c112fd19b638c39bae
Colin <ab5169@coventry.ac.uk>
2022-01-07 18:39:54 +0000

    set metabase environment variables for CI

--- dbdata/coped.backup.sql
+++ dbdata/coped.backup.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eb38498deb0d398d6da68dd2f66b21f6760c7efd343a2e62cae07ef0dbd00a02
-size 82620392
+oid sha256:dcb1c3d1ef6ce68df23d7b0023c4a773112ee2e13d04ecc4f5d370eb097e9a88
+size 82620397
