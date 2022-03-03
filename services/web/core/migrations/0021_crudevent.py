8e0e4e6976053afbfaa97b617c2d9e1e3f7b1d97
Colin <ab5169@coventry.ac.uk>
2022-03-03 20:38:51 +0000

    exclude unneeded migration dependency

--- services/web/core/migrations/0021_crudevent.py
+++ services/web/core/migrations/0021_crudevent.py
@@ -7,20 +7,30 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('easyaudit', '0016_auto_20220217_1116'),
-        ('core', '0020_remove_projectfund_currency'),
+        # ("easyaudit", "0016_auto_20220217_1116"),
+        ("core", "0020_remove_projectfund_currency"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='CRUDEvent',
+            name="CRUDEvent",
             fields=[
-                ('crudevent_ptr', models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, serialize=False, to='easyaudit.crudevent')),
-                ('object_json', models.JSONField()),
+                (
+                    "crudevent_ptr",
+                    models.OneToOneField(
+                        auto_created=True,
+                        on_delete=django.db.models.deletion.CASCADE,
+                        parent_link=True,
+                        primary_key=True,
+                        serialize=False,
+                        to="easyaudit.crudevent",
+                    ),
+                ),
+                ("object_json", models.JSONField()),
             ],
             options={
-                'db_table': 'coped_crudevent',
+                "db_table": "coped_crudevent",
             },
-            bases=('easyaudit.crudevent',),
+            bases=("easyaudit.crudevent",),
         ),
     ]
