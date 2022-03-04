ca875cf7d5594cf0a05afc8e5d25a98e3619182d
Colin <ab5169@coventry.ac.uk>
2022-03-04 18:23:29 +0000

    ensure sites framework has an id

--- services/web/core/settings.py
+++ services/web/core/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
+SITE_ID = 1
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
