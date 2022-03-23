9cd9ccb6438fe3c4db959870b7e6536d4a4ba875
Colin <ab5169@coventry.ac.uk>
2022-03-23 11:23:50 +0000

    improve management command for creating admin

--- services/web/core/management/commands/createsuperuser_for_coped.py
+++ services/web/core/management/commands/createsuperuser_for_coped.py
@@ -1,5 +1,6 @@
 import requests
 from getpass import getpass
+from django.db.models import Q
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 
@@ -8,9 +9,11 @@ METABASE_API_URL = "http://metabase:3000/api"
 
 class Command(BaseCommand):
     """
-    Create a superuser with a required email field
+    Create a superuser with required data.
+    User is created in the CoPED database and the Metabase database.
     Example:
-        manage.py createsuperuser_for_coped --user=admin --email=admin@example.com
+        manage.py createsuperuser_for_coped --username admin \
+        --email admin@example.com --first-name joe --last-name bloggs
     """
 
     def add_arguments(self, parser):
@@ -22,23 +25,10 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
 
         User = get_user_model()
-        superusers = User.objects.filter(is_superuser=True)
-        if superusers.exists():
-            self.stderr.write("There is already a superuser in the database. Aborting")
-            return None
-
-        try:
-            r = requests.get(f"{METABASE_API_URL}/session/properties")
-            r.raise_for_status()
-        except requests.exceptions.HTTPError as e:
-            self.stderr.write("Error requesting Metabase token")
-            self.stderr.write(r.text)
-            return None
-
-        setup_token = r.json().get("setup-token")
-        if not setup_token:
+        superuser_exists = User.objects.filter(is_superuser=True).exists()
+        if superuser_exists:
             self.stderr.write(
-                "No Metabase setup token available. Has Metabase already been configured?"
+                "A superuser already exists. Please log in to CoPED/Metabase to add new ones."
             )
             return None
 
@@ -61,6 +51,25 @@ class Command(BaseCommand):
             email=email,
         )
 
+        if User.objects.filter(Q(username=username) | Q(email=email)).exists():
+            self.stderr.write("Username or email already in use. Aborting")
+            return None
+
+        try:
+            r = requests.get(f"{METABASE_API_URL}/session/properties")
+            r.raise_for_status()
+        except requests.exceptions.HTTPError as e:
+            self.stderr.write("Error requesting Metabase token for initial setup.")
+            self.stderr.write(r.text)
+            return None
+
+        setup_token = r.json().get("setup-token")
+        if not setup_token:
+            self.stderr.write(
+                "No Metabase setup token received. Has Metabase already been configured?"
+            )
+            return None
+
         json_body = {
             "user": new_user,
             "prefs": {"site_name": "CoPED"},
@@ -74,9 +83,26 @@ class Command(BaseCommand):
             self.stderr.write(r.text)
             return None
 
-        metabase_id = r.json.get("id")
-        new_user = new_user.update({"metabase_id": metabase_id})
+        self.stdout.write(f"Superuser '{username}' created in Metabase database")
+
+        token = r.json().get("id")
+        auth = {"X-Metabase-Session": token}
+
+        try:
+            r = requests.get(f"{METABASE_API_URL}/user/current", headers=auth)
+            r.raise_for_status()
+        except requests.exceptions.HTTPError as e:
+            self.stderr.write(
+                "Error getting user information to set 'metabase_id' for CoPED user. Aborting."
+            )
+            self.stderr.write(
+                f"WARNING: There is now a Metabase superuser '{username}' with no corresponding CoPED superuser."
+            )
+            self.stderr.write(r.text)
+            return None
+
+        metabase_id = r.json().get("id")
+        new_user.update({"metabase_id": metabase_id})
         User.objects.create_superuser(**new_user)
 
         self.stdout.write(f"Superuser '{username}' created in CoPED database")
-        self.stdout.write(f"Superuser '{username}' created in Metabase database")
