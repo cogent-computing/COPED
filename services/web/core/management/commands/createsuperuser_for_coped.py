5c55e975696317e707e0cde923085709b8ac883e
Colin <ab5169@coventry.ac.uk>
2022-03-23 09:34:37 +0000

    management command creates metabase superuser

--- services/web/core/management/commands/createsuperuser_for_coped.py
+++ services/web/core/management/commands/createsuperuser_for_coped.py
@@ -1,6 +1,9 @@
+import requests
 from getpass import getpass
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
+
+METABASE_API_URL = "http://metabase:3000/api"
 
 
 class Command(BaseCommand):
@@ -13,15 +16,61 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument("--username", required=True)
         parser.add_argument("--email", required=True)
+        parser.add_argument("--first-name", required=True)
+        parser.add_argument("--last-name", required=True)
 
     def handle(self, *args, **options):
 
         User = get_user_model()
+        superusers = User.objects.filter(is_superuser=True)
+        if superusers.exists():
+            self.stderr.write("There is already a superuser in the database. Aborting")
+            return None
+
+        try:
+            r = requests.get(f"{METABASE_API_URL}/session/properties")
+            r.raise_for_status()
+        except requests.exceptions.HTTPError as e:
+            self.stderr.write("Error requesting Metabase token")
+            self.stderr.write(e)
+            return None
+
+        setup_token = r.json().get("setup-token")
+        if not setup_token:
+            self.stderr.write(
+                "No Metabase setup token available. Has Metabase already been configured?"
+            )
+            return None
 
         username = options["username"]
         email = options["email"]
-        password = getpass()
+        first_name = options["first_name"]
+        last_name = options["last_name"]
+        password1 = getpass()
+        password2 = getpass(prompt="Confirm password:")
 
-        User.objects.create_superuser(username=username, password=password, email=email)
+        if password1 != password2:
+            self.stderr.write("Passwords do not match. Aborting.")
+            return None
 
-        self.stdout.write(f'Superuser "{username}" was created')
+        new_user = dict(
+            username=username,
+            password=password1,
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+        )
+
+        User.objects.create_superuser(**new_user)
+        self.stdout.write(f"Superuser '{username}' created in CoPED database")
+
+        auth = {"X-Metabase-Session": setup_token}
+        try:
+            r = requests.post(f"{METABASE_API_URL}/setup", json=new_user, headers=auth)
+            r.raise_for_status()
+        except requests.exceptions.HTTPError as e:
+            self.stderr.write("Error creating Metabase superuser")
+            self.stderr.write(e)
+            return None
+
+        self.stdout.write(f"Superuser '{username}' created in Metabase database")
