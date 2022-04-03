7b6fe535f647fbf504cbdbaa7278dfff48c651d1
Colin <ab5169@coventry.ac.uk>
2022-04-03 15:40:25 +0100

    set logging level via environment variable

--- services/web/entrypoint.sh
+++ services/web/entrypoint.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-echo "Setting environment"
-ENV="${ENVIRONMENT:-PRODUCTION}"
-echo "Environment set to ${ENVIRONMENT}"
-
 echo "Creating database migrations..."
 python manage.py makemigrations --noinput
 echo "Migrations created"
@@ -25,11 +21,11 @@ python manage.py createcachetable
 echo "Cache table created"
 
 echo "Starting celery worker daemon..."
-celery --app core worker --task-events --loglevel info --pool=threads --logfile /var/log/coped/celery_worker.log --detach
+celery --app core worker --task-events --loglevel ${LOG_LEVEL:-INFO} --pool=threads --logfile /var/log/coped/celery_worker.log --detach
 echo "Celery worker daemon started. Logs will go to /var/log/coped/celery_worker.log"
 
 echo "Starting celery beat/scheduler daemon..."
-celery --app core beat --loglevel info --scheduler django_celery_beat.schedulers:DatabaseScheduler --logfile /var/log/coped/celery_beat.log --detach
+celery --app core beat --loglevel ${LOG_LEVEL:-INFO} --scheduler django_celery_beat.schedulers:DatabaseScheduler --logfile /var/log/coped/celery_beat.log --detach
 echo "Celery beat/scheduler daemon started. Logs will go to /var/log/coped/celery_beat.log"
 
 exec "$@"
