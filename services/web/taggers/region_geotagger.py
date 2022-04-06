b1eff4eefc7767ff843dfbdc36336f6d9c81ce5d
Colin <ab5169@coventry.ac.uk>
2022-04-06 11:21:29 +0100

    formatting

--- services/web/taggers/region_geotagger.py
+++ services/web/taggers/region_geotagger.py
@@ -4,7 +4,6 @@ import sys
 import django
 import fiona
 import shapely.geometry
-from pyproj import CRS, Transformer
 from operator import itemgetter
 from celery import shared_task
 
@@ -13,6 +12,7 @@ sys.path.append(os.path.abspath(".."))
 django.setup()
 
 from core.models import GeoData
+
 
 @shared_task(name="Automatic DNO region tagger")
 def tag_geo_points_with_dno_regions(exclude_already_tagged=True, limit=None):
@@ -24,33 +24,46 @@ def tag_geo_points_with_dno_regions(exclude_already_tagged=True, limit=None):
 
     logging.info("Tagging points with DNO region ids")
     dirname = os.path.dirname(__file__)
-    geojson_filename = os.path.join(dirname, "geojson/dno_license_areas_20200506.polar.geojson")
+    geojson_filename = os.path.join(
+        dirname, "geojson/dno_license_areas_20200506.polar.geojson"
+    )
     with fiona.open(geojson_filename) as fiona_collection:
 
         for geo_point in geo_points[:limit]:
             logging.debug("point %s", geo_point)
 
             lat, lon = geo_point.lat, geo_point.lon
-            query_point = shapely.geometry.Point(lon,lat)
+            query_point = shapely.geometry.Point(lon, lat)
             logging.debug("query point is %s", query_point)
 
             for shapefile_record in iter(fiona_collection):
-                record_type, record_id, record_properties, record_geometry = itemgetter("type", "id", "properties", "geometry")(shapefile_record)
+                record_type, record_id, record_properties, record_geometry = itemgetter(
+                    "type", "id", "properties", "geometry"
+                )(shapefile_record)
 
-                shape = shapely.geometry.shape( record_geometry )
+                shape = shapely.geometry.shape(record_geometry)
 
                 if shape.contains(query_point):
                     dno_id = record_properties.get("ID")
                     dno_name = record_properties.get("Name")
                     dno_long_name = record_properties.get("LongName")
 
-                    logging.info("DNO %s contains test point %s (%s, %s)", dno_id, geo_point.id, lat, lon)
+                    logging.info(
+                        "DNO %s contains test point %s (%s, %s)",
+                        dno_id,
+                        geo_point.id,
+                        lat,
+                        lon,
+                    )
 
                     geo_point.dno_id = dno_id
                     geo_point.dno_name = dno_name
                     geo_point.dno_long_name = dno_long_name
-                    geo_point.save(update_fields=["dno_id", "dno_name", "dno_long_name"])
+                    geo_point.save(
+                        update_fields=["dno_id", "dno_name", "dno_long_name"]
+                    )
                     break
+
 
 if __name__ == "__main__":
     tag_geo_points_with_dno_regions(exclude_already_tagged=True, limit=10)
